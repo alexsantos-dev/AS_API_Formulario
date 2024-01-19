c2fca74b24760d027367eb5c9df41d0ac4810f0e
Δlex Santos <140655997+alexsantos-dev@users.noreply.github.com>
2024-01-19 13:56:31 -0300

    adiciondo keep-alive

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ app.use("/api", FormRoutes);
 
 const PORT = process.env.PORT || 3000
 
+async function api() {
+    const accessUrl = async () => {
+        try {
+            await axios.get('https://auth-user-api.onrender.com');
+        } catch (error) {
+            console.error(error.message);
+            setInterval(accessUrl, 5000);
+        }
+    };
+
+    setInterval(accessUrl, 300000);
+
+
+    await new Promise(() => { });
+}
+
+api();
+
 app.listen(PORT, () => {
     console.log('Conexão bem sucedida! ✅');
     console.log(`Servidor ativo em: http://localhost:${PORT} 📎`);
