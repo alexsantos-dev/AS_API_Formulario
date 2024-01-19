d6e92a1bcacc4fc6f3e82991a52c1360bbdf0e03
Δlex Santos <140655997+alexsantos-dev@users.noreply.github.com>
2024-01-19 17:08:04 -0300

    ajustando cors

--- src/routes/formRoutes.js
+++ src/routes/formRoutes.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const router = express.Router();
 
 const corsOptions = {
-    origin: ['http://localhost:5500', 'http://127.0.0.1', 'https://auth-user-api.onrender.com/api/forms'],
+    origin: ['http://localhost:5500', 'http://127.0.0.1', 'https://auth-user-api.onrender.com/api/forms', 'https://auth-user-api.onrender.com/api/forms/auth'],
     methods: 'GET,HEAD,PATCH,PUT,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
