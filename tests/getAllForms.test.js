2b317ff2adc7e94a16e44ea90d32eae3f0a4d73a
Alex Santos <alexsantos.web.developer@gmail.com>
2024-01-21 18:41:23 -0300

    ajuste no teste da rota forms

--- tests/getAllForms.test.js
+++ tests/getAllForms.test.js
@@ -4,15 +4,10 @@ import app from '../index.js';
 describe('GET /api/forms', () => {
     it('should call getAllForms controller', async () => {
         const response = await request(app).get('/api/forms');
-
         expect(response.status).toBe(200);
     });
-});
-
-describe('GET /api/forms', () => {
     it('should call getAllForms controller', async () => {
-        const response = await request(app).get('/api/flings');
-
+        const response = await request(app).get('/api/orms');
         expect(response.status).toBe(404);
     });
 });
